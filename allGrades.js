69ceec72239343fd254df87c6f495c64539d6397
Sam Bennett <SamBennettDev@gmail.com>
2024-01-02 20:43:32 -0700

    Update allGrades.js

--- allGrades.js
+++ allGrades.js
@@ -14,27 +14,39 @@ const corsProxyLink = "";
 // Ex ["625183", "123456", "718391"]
 const courses = ["", "", "", "", ""];
 
+
+
+
 async function fetchCourseGrade(courseId) {
   const apiURL = `${schoolCanvasLink}/api/v1/courses/${courseId}?access_token=${accessToken}&include[]=total_scores`;
   const url = `${corsProxyLink}${apiURL}`;
-
+  
   try {
     xhr = new XMLHttpRequest();
     xhr.open("GET", url, false);
     xhr.send(null);
     const data = JSON.parse(xhr.responseText);
     const currentScore = data.enrollments[0].computed_current_score;
-    return currentScore !== null ? currentScore : "-----";
+
+    if (currentScore !== null) {
+      // Check if the score is greater than or equal to 100
+      if (currentScore >= 100) {
+        // Round to the tenth place
+        return Math.round(currentScore * 10) / 10;
+      } else {
+        return currentScore;
+      }
+    } else {
+      return "-----";
+    }
   } catch (error) {
-    return "N/A";
+    return "NE";
   }
 }
-
 async function fetchAllCourseGrades() {
   const gradePromises = courses.map(fetchCourseGrade);
   const grades = await Promise.all(gradePromises);
   const formattedGrades = `${grades.join("\n\n")}`;
   sendToWidgy(formattedGrades);
 }
-
 fetchAllCourseGrades();
